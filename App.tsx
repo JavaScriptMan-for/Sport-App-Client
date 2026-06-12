bdb41d093989e59404c73580f8f6de5f891cf6df
JavaScriptMan <minevskiia10@gmail.com>
2026-06-12 21:27:01 +0300

    api

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import Navigation from "./Navigation";
 
 
+
 export default function App() {
   return (
     <Provider store={store}>
